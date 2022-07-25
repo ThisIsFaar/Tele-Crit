24a275a562df14708a0b7da6e4e53ac9e84f1581
thisisfaar <thisisfarhan99@gmail.com>
2022-07-25 19:02:46 +0530

    retrigger

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,7 +3,7 @@ const { validationResult } = require('express-validator');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 
-exports.signin = (req, res) => {
+exports.signin = async (req, res) => {
   const errors = validationResult(req);
   const { username, password } = req.body;
 
@@ -14,37 +14,31 @@ exports.signin = (req, res) => {
     });
   }
 
-  User.findOne({ username }, async (err, user) => {
-    console.log(
-      '::::::::::::::::::::::::username:::::::::::::::',
-      username,
-      err,
-      user
-    );
-    if (err || !user) {
-      return res.status(403).json({
-        message: 'Username Does Not Exists',
-        code: 403,
-        err: err,
-        user: user,
-      });
-    }
-    const checkPassword = await bcrypt.compare(password, user.password);
+  const user = await User.findOne({ username });
+  console.log('::::::::::::::::user:::::::', user);
+  // if (err || !user) {
+  //   return res.status(403).json({
+  //     message: 'Username Does Not Exists',
+  //     code: 403,
+  //     err: err,
+  //     user: user,
+  //   });
+  // }
+  const checkPassword = await bcrypt.compare(password, user.password);
 
-    if (checkPassword) {
-      const { _id } = user;
-      const token = jwt.sign({ _id: user._id }, process.env.SECRET);
+  if (checkPassword) {
+    const { _id } = user;
+    const token = jwt.sign({ _id: user._id }, process.env.SECRET);
 
-      return res.status(200).json({
-        token,
-        user: { _id },
-        code: 200,
-      });
-    } else {
-      return res.status(401).json({
-        message: 'Username Password does not match',
-        code: 401,
-      });
-    }
-  });
+    return res.status(200).json({
+      token,
+      user: { _id },
+      code: 200,
+    });
+  } else {
+    return res.status(401).json({
+      message: 'Username Password does not match',
+      code: 401,
+    });
+  }
 };
