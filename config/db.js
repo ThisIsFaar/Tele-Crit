cab0c58765616579bb43b3f927e36997aec524b8
thisisfaar <thisisfarhan99@gmail.com>
2022-07-25 21:16:31 +0530

    retrigger

--- config/db.js
+++ config/db.js
@@ -4,9 +4,12 @@ require('dotenv').config();
 const connectDB = async () => {
   const db = process.env.DB_AUTH;
   try {
-    await mongoose.connect(db, {
-      useNewUrlParser: true,
-    });
+    await mongoose.connect(
+      'mongodb+srv://farhan0987:farhan0987@cluster.swtnceq.mongodb.net/?retryWrites=true&w=majority',
+      {
+        useNewUrlParser: true,
+      }
+    );
 
     console.log('MongoDB is Connected...');
   } catch (err) {
